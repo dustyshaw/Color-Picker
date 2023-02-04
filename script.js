72a20bcef6881f5a1e53c1d4a31d7604cefe2296
Dusty Shaw <shustydaw@gmail.com>
2023-02-04 13:41:13 -0700

    Need to fix overflow vals

--- script.js
+++ script.js
@@ -10,22 +10,39 @@ base.addEventListener("change", function () {
     var r = parseInt(hexCode.substring(0, 2), 16);
     var g = parseInt(hexCode.substring(2, 4), 16);
     var b = parseInt(hexCode.substring(4, 6), 16);
+
+    ComputeComplimentary(r, g, b);
+    ComputeTertiary(r, g, b);
+});
+
+function ComputeComplimentary(r, g, b) {
     var rComp = 255 - r;
     var gComp = 255 - g;
     var bComp = 255 - b;
-    color2.style.backgroundColor = color;
-    color3.style.backgroundColor = `rgb(${r * 1.5}, ${g * 1.5}, ${b * 1.5})`;
-    color1.style.backgroundColor = `rgb(${(r * .8)}, ${(g * .8)}, ${(b * .8)})`;
-    color5.style.backgroundColor = `rgb(${rComp}, ${gComp}, ${bComp})`;
-    color4.style.backgroundColor = `rgb(${rComp * 1.5}, ${gComp * 1.5}, ${bComp * 1.5})`;
-    ncolor1 = `rgb(${(r * .8)}, ${(g * .8)}, ${(b * .8)})`;
-    ncolor2 = color;
-    ncolor3 = `rgb(${r * 1.5}, ${g * 1.5}, ${b * 1.5})`;
-    ncolor4 = `rgb(${rComp * 1.5}, ${gComp * 1.5}, ${bComp * 1.5})`;
-    ncolor5 = `rgb(${rComp}, ${gComp}, ${bComp})`;
+    var ncolor1 = `rgb(${Math.round(r * .8)}, ${Math.round(g * .8)}, ${Math.round(b * .8)})`;
+    var ncolor2 = `rgb(${Math.round(r)}, ${Math.round(g)}, ${Math.round(b)})`;
+    var ncolor3 = `rgb(${Math.round(r * 1.5)}, ${Math.round(g * 1.5)}, ${Math.round(b * 1.5)})`;
+    var ncolor4 = `rgb(${Math.round(rComp * 1.5)}, ${Math.round(gComp * 1.5)}, ${Math.round(bComp * 1.5)})`;
+    var ncolor5 = `rgb(${Math.round(rComp)}, ${Math.round(gComp)}, ${Math.round(bComp)})`;
+    color2.style.backgroundColor = ncolor2;
+    color3.style.backgroundColor = ncolor3;
+    color1.style.backgroundColor = ncolor1;
+    color5.style.backgroundColor = ncolor5;
+    color4.style.backgroundColor = ncolor4;
+
     UpdatePage(ncolor1, ncolor2, ncolor3, ncolor4, ncolor5);
-    color1.innerText = `rgb(${Math.round((r * .8))}, ${Math.round((g * .8))}, ${Math.round((b * .8))})`;
-});
+    color1.innerText = ncolor1;
+    color2.innerText = ncolor2;
+    color3.innerText = ncolor3;
+    color4.innerText = ncolor4;
+    color5.innerText = ncolor5;
+}
+
+function ComputeTertiary(r, g, b) {
+    var rTert = 255 - r;
+    var gTert = 255 - g;
+    var bTert = 255 - b;
+}
 
 function UpdatePage(color1, color2, color3, color4, color5) {
     var h1s = document.querySelectorAll('h1');
@@ -38,18 +55,10 @@ function UpdatePage(color1, color2, color3, color4, color5) {
     });
     var h3s = document.querySelectorAll('h3');
     h3s.forEach(element => {
-        element.style.color = color3;
+        element.style.color = color4;
     });
-    var radios = document.getElementsByClassName('radio');
-    for (i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            //radios[i].style.accentColor = color3;
-        }
+    var gradients = document.getElementsByClassName('gradient');
+    for (i = 0; i < gradients.length; i++) {
+        gradients[i].style.backgroundImage = `linear-gradient(to right, ${color4}, ${color5})`;
     }
-    var buttons = document.querySelectorAll('button');
-    buttons.forEach(element => {
-        element.style.backgroundImage = `linear-gradient(to right, ${color4}, ${color5})`;
-        element.style.color = 'white';
-    });
-
 }
